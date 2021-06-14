5472e969ed495b62cb87e8268e091d32fdec4438
tmb <tom.brooke@gmail.com>
2021-06-14 08:55:43 -0400

    added experiments with cicero server

--- src/trustblocks/cicero.clj
+++ src/trustblocks/cicero.clj
@@ -1,5 +1,6 @@
 (ns trustblocks.cicero
   (:require [biff.util :as bu]
+            [clj-http.client :as client]
             [portal.api :as p]
             [clojure.string :as str]
             [clojure.data.json :as json]))
@@ -11,13 +12,37 @@
     (bu/sh "cicero" "parse" cta :out-enc "UTF-8")))
 
 
-
+;;;  Version 2 using cicero-server --- running locally Localhost:6001
 
 (comment
 
+  ;;; Grab a json model from the model repository
+ (client/get "https://models.accordproject.org/accordproject/party.json")
+
+  (client/post "http://localhost:6001/parse/latedeliveryandpenalty"
+               {:content-type :json
+                :accept       :json
+                :body-content   {"sample" " ##Late Delivery and Penalty. \n \n In case of delayed delivery except for Force Majeure cases, \n \"Dan \" (the Seller) shall pay to \"Steve \" (the Buyer) for every 2 days \nof delay penalty amounting to 10.5 % of the total value of the Equipment \nwhose delivery has been delayed. Any fractional part of a days is to be \nconsidered a full days. The total amount of penalty shall not however, \nexceed 55 % of the total value of the Equipment involved in late delivery. \nIf the delay is more than 15 days, the Buyer is entitled to terminate this Contract." }
+                })
+
+  curl --request POST \
+  --url http://localhost:6001/parse/latedeliveryandpenalty \
+  --header 'accept: application/json' \
+  --header 'content-type: application/json' \
+  --data '{
+           "sample": "## Late Delivery and Penalty.\n\n In case of delayed delivery except for Force Majeure cases,\n\"Dan\" (the Seller) shall pay to \"Steve\" (the Buyer) for every 2 days\nof delay penalty amounting to 10.5% of the total value of the Equipment\nwhose delivery has been delayed. Any fractional part of a days is to be\nconsidered a full days. The total amount of penalty shall not however,\nexceed 55% of the total value of the Equipment involved in late delivery.\nIf the delay is more than 15 days, the Buyer is entitled to terminate this Contract." }
+  '
+
+
+  )
+
+
+(comment  -- CLI Tests
+  (run-cicero "promissory-note")
 (p/open)                                                    ;Open Portal Browser
 (add-tap #'p/submit)                                        ;add p as portal target
-(def k (run-cicero "promissory-note"))                      ; returns text with two lines at front
+(def k (run-cicero "promissory-note"))  ; returns text with two lines at front
+(println k)
 (tap> (run-cicero "promissory-note"))                       ; have to strip off 1st 2 lines
 (println (str/trim-newline (run-cicero "promissory-note"))) ; doesn't work returns whole thing in vector
 (println k)
